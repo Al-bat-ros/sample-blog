0051f1a50805859d1a6d17c6c6d9ab438b33432e
Al-bat-ros <al-bat-ros@mail.ru>
2018-05-04 02:42:19 +0500

    Add /terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get '/' => 'home#index'
 
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
-  get '/' => 'home#index'
+
+  resource :terms, only: [:new], path_names: {:new => ''}
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
