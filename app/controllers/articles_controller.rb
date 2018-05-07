e4cf189d9c8cb409f6e457cdcbc6aec39734827a
Al-bat-ros <al-bat-ros@mail.ru>
2018-05-08 03:46:43 +0500

    implement articles#edit articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,14 +14,29 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-      if @article.valid?
-         @article.save
+
+      if @article.save
          redirect_to @article
       else
         render action: 'new'
       end
       #вывод передоваемой инфы в представление
     #render plain: params[:article].inspect
+  end
+
+  def edit
+    @article = Article.find(params[:id])  
+  end
+
+  def update
+    @article = Article.find(params[:id]) 
+
+      if @article.update(article_params)
+         
+         redirect_to @article
+      else
+        render action: 'edit'
+      end
   end
 
   private
