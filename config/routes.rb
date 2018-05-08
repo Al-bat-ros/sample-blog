6562582a587476826c6f115973329bb4631c3f79
Al-bat-ros <al-bat-ros@mail.ru>
2018-05-09 00:08:48 +0500

    Add terms end about pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
   get '/' => 'home#index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+  
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
 
-  resource :terms, only: [:new], path_names: {:new => ''}
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
