d78304bd0646878f9e6c36ba4db469c3bdd408fe
Al-bat-ros <al-bat-ros@mail.ru>
2018-05-12 03:33:11 +0500

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
-  
+
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  
+  resources :articles do
+    resources :comments
+  end
 
 
   
