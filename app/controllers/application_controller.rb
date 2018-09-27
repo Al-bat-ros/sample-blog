02a1586edfcc99508edd51e7cef170bd437810ea
Al-bat-ros <al-bat-ros@mail.ru>
2018-09-27 21:34:21 +0500

    Permit username for sign_up form 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
-  before_filter :configure_permitted_parameters, if: :devise_controller? 
-    
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+
   private
 
   def configure_permitted_parameters
